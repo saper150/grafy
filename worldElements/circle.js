4659d10f36671b0ebfbd99fa6296fbe23c6a6e11
wojtix144 <wojtix144@gmail.com>
2017-10-28 16:19:02 +0200

    deleted leftovers

--- worldElements/circle.js
+++
@@ -1,9 +0,0 @@
-const Box2D = require('box2dweb')
-const WorldElement = require('./worldElement')
-module.exports = class Circle extends WorldElement {
-    constructor(world, [x, y], r) {
-        let shape = new Box2D.Collision.Shapes.b2CircleShape
-        shape.m_radius = r
-        super(world, [x, y], shape)
-    }
-}
